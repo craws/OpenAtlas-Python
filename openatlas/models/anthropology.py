187bac8ad4558d007da30d02f5108130a1f3ee07
Alexander Watzinger <alexander.watzinger@craws.net>
2021-05-07 17:46:07 +0200

    Sex estimation feature with Nina

--- openatlas/models/anthropology.py
+++ openatlas/models/anthropology.py
@@ -1,6 +1,13 @@
 from typing import Dict
 
+from flask import g
+
 from openatlas.models.entity import Entity
+
+# Todo:
+# - if saved one of the sex features types are shown as default type
+# - if entity is updated, feature types get deleted
+# - remove default
 
 
 class SexEstimation:
@@ -16,47 +23,147 @@ class SexEstimation:
 
     features = {
         'Skull': {
-            'Glabella': 3,
-            'Arcus superciliaris': 2,
-            'Tuber frontalis and parietalis': 2,
-            'Inclinatio frontalis': 1,
-            'Processus mastoideus': 3,
-            'Relief of planum nuchale': 3,
-            'Protuberantia occipitalis externa': 2,
-            'Processus zygomaticus': 3,
-            'Os zygomaticum': 2,
-            'Crista supramastoideum': 2,
-            'Margo supraorbitalis': 1,
-            'Shape of orbita': 1},
+            'Glabella': {
+                'value': 3,
+                'female': 'smooth, non-projecting',
+                'male': 'prominent, projecting'},
+            'Arcus superciliaris': {
+                'value': 2,
+                'female': '',
+                'male': ''},
+            'Tuber frontalis and parietalis': {
+                'value': 2,
+                'female': '',
+                'male': ''},
+            'Inclinatio frontalis': {
+                'value': 1,
+                'female': '',
+                'male': ''},
+            'Processus mastoideus': {
+                'value': 3,
+                'female': '',
+                'male': ''},
+            'Relief of planum nuchale': {
+                'value': 3,
+                'female': '',
+                'male': ''},
+            'Protuberantia occipitalis externa': {
+                'value': 2,
+                'female': '',
+                'male': ''},
+            'Processus zygomaticus': {
+                'value': 3,
+                'female': '',
+                'male': ''},
+            'Os zygomaticum': {
+                'value': 2,
+                'female': '',
+                'male': ''},
+            'Crista supramastoideum': {
+                'value': 2,
+                'female': '',
+                'male': ''},
+            'Margo supraorbitalis': {
+                'value': 1,
+                'female': '',
+                'male': ''},
+            'Shape of orbita': {
+                'value': 1,
+                'female': '',
+                'male': ''}},
         'Mandible': {
-            'Overall apperence': 3,
-            'Mentum': 2,
-            'Angulus': 1,
-            'Margo inferior (M2)': 1,
-            'Angle': 1},
+            'Overall apperence': {
+                'value': 3,
+                'female': '',
+                'male': ''},
+            'Mentum': {
+                'value': 2,
+                'female': '',
+                'male': ''},
+            'Angulus': {
+                'value': 1,
+                'female': '',
+                'male': ''},
+            'Margo inferior (M2)': {
+                'value': 1,
+                'female': '',
+                'male': ''},
+            'Angle': {
+                'value': 1,
+                'female': '',
+                'male': ''}},
         'Pelvis': {
-            'Sulcus praeauricularis': 3,
-            'Incisura ischiadica major': 3,
-            'Angulus pubis': 2,
-            'Arc composé': 2,
-            'Os coxae': 2,
-            'Foramen obturatum': 2,
-            'Corpus ossis ischii': 2,
-            'Crista iliaca': 1,
-            'Fossa iliaca': 1,
-            'Pelvis major': 1,
-            'Auricular area': 1,
-            'Sacrum': 1,
-            'Fossa acetabuli': 1},
+            'Sulcus praeauricularis': {
+                'value': 3,
+                'female': '',
+                'male': ''},
+            'Incisura ischiadica major': {
+                'value': 3,
+                'female': '',
+                'male': ''},
+            'Angulus pubis': {
+                'value': 2,
+                'female': '',
+                'male': ''},
+            'Arc composé': {
+                'value': 2,
+                'female': '',
+                'male': ''},
+            'Os coxae': {
+                'value': 2,
+                'female': '',
+                'male': ''},
+            'Foramen obturatum': {
+                'value': 2,
+                'female': '',
+                'male': ''},
+            'Corpus ossis ischii': {
+                'value': 2,
+                'female': '',
+                'male': ''},
+            'Crista iliaca': {
+                'value': 1,
+                'female': '',
+                'male': ''},
+            'Fossa iliaca': {
+                'value': 1,
+                'female': '',
+                'male': ''},
+            'Pelvis major': {
+                'value': 1,
+                'female': '',
+                'male': ''},
+            'Auricular area': {
+                'value': 1,
+                'female': '',
+                'male': ''},
+            'Sacrum': {
+                'value': 1,
+                'female': '',
+                'male': ''},
+            'Fossa acetabuli': {
+                'value': 1,
+                'female': '',
+                'male': ''}},
         'Robusticity': {
-            'Humerus': 1,
-            'Femur': 1}}
+            'Humerus': {
+                'value': 1,
+                'female': '',
+                'male': ''},
+            'Femur': {
+                'value': 1,
+                'female': '',
+                'male': ''}}}
 
     @staticmethod
-    def save(entity: Entity, data: Dict[str, int]) -> None:
-        print(f'Calculating sex estimation for {entity.name}')
+    def get_by_name(feature_name):
+        for group in SexEstimation.features.values():
+            for name, values in group.items():
+                if name == feature_name:
+                    return values['type_id']
+
+    @staticmethod
+    def save(entity: Entity, data: Dict[str, str]) -> None:
         for key, item in data.items():
-            print(key)
-            print(item)
-            print(SexEstimation.options[item])
+            entity.link('P2', g.nodes[SexEstimation.get_by_name(key)], item)
         print(f'This values {data}')
--- openatlas/views/anthropology.py
+++ openatlas/views/anthropology.py
@@ -1,14 +1,16 @@
 from typing import Union
 
-from flask import render_template, url_for
+from flask import g, render_template, url_for
 from flask_babel import lazy_gettext as _
 from flask_wtf import FlaskForm
+from werkzeug.utils import redirect
 from werkzeug.wrappers import Response
 from wtforms import SelectField, SubmitField
 
 from openatlas import app
 from openatlas.models.anthropology import SexEstimation
 from openatlas.models.entity import Entity
+from openatlas.models.node import Node
 from openatlas.util.util import required_group, uc_first
 
 
@@ -25,7 +27,7 @@ def anthropology_index(id_: int) -> Union[str, Response]:
 @app.route('/anthropology/sex/<int:id_>')
 @required_group('readonly')
 def anthropology_sex(id_: int) -> Union[str, Response]:
-    entity = Entity.get_by_id(id_)
+    entity = Entity.get_by_id(id_, nodes=True)
     return render_template(
         'anthropology/sex.html',
         entity=entity,
@@ -39,16 +41,34 @@ def anthropology_sex(id_: int) -> Union[str, Response]:
 @required_group('contributor')
 def anthropology_sex_update(id_: int) -> Union[str, Response]:
 
+    # Build the form with selects for features
     class Form(FlaskForm):  # type: ignore
         pass
+
     choices = [(option, option) for option in SexEstimation.options.keys()]
     for features in SexEstimation.features.values():
-        for feature in features.keys():
+        for feature, values in features.items():
             label = uc_first(feature.replace('_', ' '))
-            setattr(Form, feature, SelectField(label, choices=choices))
-
+            description = ''
+            if values['female'] or values['male']:
+                description = f"Female: {values['female']}, male: {values['male']}"
+            setattr(Form,
+                    feature,
+                    SelectField(
+                        label,
+                        choices=choices,
+                        default='Not preserved',
+                        description=description))
     setattr(Form, 'save', SubmitField(_('save')))
     form = Form()
+
+    #  Add type information to features
+    sex_node_subs = Node.get_nodes('Features for sexing')
+    for group_id in sex_node_subs:
+        group = g.nodes[group_id]
+        for node_id in group.subs:
+            node = g.nodes[node_id]
+            SexEstimation.features[group.name][node.name]['type_id'] = node.id
 
     entity = Entity.get_by_id(id_)
     if form.validate_on_submit():
@@ -56,6 +76,7 @@ def anthropology_sex_update(id_: int) -> Union[str, Response]:
         data.pop('save')
         data.pop('csrf_token')
         SexEstimation.save(entity, data)
+        return redirect(url_for('anthropology_sex', id_=entity.id))
 
     return render_template(
         'anthropology/sex_update.html',
